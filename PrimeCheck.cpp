4f27b6d760d534946d49326c96f6bd2575de4371
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-01-21 18:48:20 +0530

    Update PrimeCheck.cpp

--- PrimeCheck.cpp
+++ PrimeCheck.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main() {
     int n;
-    cin>>n;
+    cin >> n;
 
-    bool isPrime=true;
-    for(int i=2;i<=n/2;i++) {
-        if(n%i==0) {
-            isPrime=false;
+    if (n == 0 || n == 1) {
+        cout << "Neither Prime nor Composite";
+        return 0;
+    }
+
+    bool isPrime = true;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            isPrime = false;
             break;
         }
     }
 
-    if(isPrime) {
-        if(n==1 || n==0) {
-            cout<<"Neither Prime nor Composite";
-        } else {
-            cout<<"Prime";
-        }
-    }
-    else {
-        cout<<"Not Prime";
+    if (isPrime) {
+        cout << "Prime";
+    } else {
+        cout << "Not Prime";
     }
 
     return 0;
