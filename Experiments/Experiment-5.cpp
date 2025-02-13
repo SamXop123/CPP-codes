5c837d03b24a8793325046acc0b7b8d600b8c773
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-02-13 23:00:02 +0530

    Create Experiment-5.cpp

---
+++ Experiments/Experiment-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// Inline function to calculate the square of a number
+inline int square(int x) {
+    return x * x;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Square of " << num << " is: " << square(num) << endl;
+
+    return 0;
+}
