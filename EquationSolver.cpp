2293efa93dbd15d9191b7e7c8f83a24844097503
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-02-18 18:34:18 +0530

    Update EquationSolver.cpp

--- EquationSolver.cpp
+++ EquationSolver.cpp
@@ -1,51 +1,49 @@
 #include <iostream>
-#include <cmath> // Use <cmath> instead of <math.h>
+#include <cmath>
 
 using namespace std;
 
 class Quadratic {
-    double Discriminant(); 
-    void findRoots(); // Changed return type to void
-
 public:
-    double a, b, c;
+    double a, b, c, d;
 
     Quadratic() {
         cout << "Enter the variables in this format: a(x)^2 + bx + c = 0" << endl;
     }
 
     void InputVariables() { 
-        cout << "Enter a:" << endl;
+        cout << "Enter a: ";
         cin >> a;
-        cout << "Enter b:" << endl;
+        cout << "Enter b: ";
         cin >> b;
-        cout << "Enter c:" << endl;
+        cout << "Enter c: ";
         cin >> c;
     }
+
+    double Discriminant() {  
+        return (b * b) - (4 * a * c);
+    }
+
+    void findRoots() {
+        d = Discriminant();  
+    
+        if (d > 0) {
+            double r1 = (-b + sqrt(d)) / (2 * a);
+            double r2 = (-b - sqrt(d)) / (2 * a);
+            cout << "Roots are real and distinct: " << r1 << " and " << r2 << endl;
+        } 
+        else if (d == 0) {
+            double r = -b / (2 * a);
+            cout << "Roots are real and equal: " << r << endl;
+        } 
+        else {
+            double realPart = -b / (2 * a);
+            double imaginaryPart = sqrt(-d) / (2 * a);
+            cout << "Roots are complex: " << realPart << " ± " << imaginaryPart << "i" << endl;
+        }
+    }
+    
 };
-
-double Quadratic::Discriminant() {
-    return (b * b) - (4 * a * c); 
-}
-
-void Quadratic::findRoots() {
-    double d = Discriminant(); // Call Discriminant() instead of using a non-existent variable
-
-    if (d > 0) {
-        double r1 = (-b + sqrt(d)) / (2 * a);
-        double r2 = (-b - sqrt(d)) / (2 * a);
-        cout << "Roots are real and distinct: " << r1 << " and " << r2 << endl;
-    }
-    else if (d == 0) {
-        double r = -b / (2 * a);
-        cout << "Roots are real and equal: " << r << endl;
-    }
-    else {
-        double realPart = -b / (2 * a);
-        double imaginaryPart = sqrt(-d) / (2 * a);
-        cout << "Roots are complex: " << realPart << " ± " << imaginaryPart << "i" << endl;
-    }
-}
 
 int main() {
     cout << "======WELCOME TO THE ULTIMATE QUADRATIC EQUATIONS SOLVER======" << endl;
@@ -55,7 +53,7 @@ int main() {
     q.InputVariables();
 
     double d_val = q.Discriminant();
-    cout << "Discriminant: " << d_val << endl; 
+    cout << "Discriminant: " << d_val << endl;  
 
     if (d_val < 0) {
         cout << "The discriminant is negative, meaning the solutions are imaginary." << endl;
@@ -63,14 +61,14 @@ int main() {
         cin >> option;
 
         if (option == 'Y' || option == 'y') {
-            q.findRoots(); 
+            q.findRoots();  
         }
         else {
             cout << "Thank you for using our solver! See you next time!" << endl;
         }
     }
     else {
-        q.findRoots();
+        q.findRoots();  
     }
 
     return 0; 
