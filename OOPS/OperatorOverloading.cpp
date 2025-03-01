ff57f07cab19f2f5305c33e5d1ceb04fb5f2573a
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-03-01 19:19:10 +0530

    Create OperatorOverloading.cpp

---
+++ OOPS/OperatorOverloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+    int real, imag;
+
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    // overloading the + operator
+    Complex operator+(const Complex &obj) {
+        Complex temp;
+        temp.real = real + obj.real;
+        temp.imag = imag + obj.imag;
+        return temp;
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(3, 4), c2(1, 2);
+    Complex c3 = c1 + c2;  // using overloaded + operator
+    c3.display(); 
+
+    return 0;
+}
