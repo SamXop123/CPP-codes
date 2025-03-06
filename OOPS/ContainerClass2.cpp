413cfa23e2ca254d36820008e803422f891a87ac
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-03-07 01:36:26 +0530

    Create ContainerClass2.cpp

---
+++ OOPS/ContainerClass2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Item {
+    int value;
+
+public:
+    Item(int v) { value = v; }
+    void display() { cout << "Item Value: " << value << endl; }
+};
+
+// container class
+class Container {
+    Item item1;
+    Item item2;
+
+public:
+    Container(int v1, int v2) : item1(v1), item2(v2) {}
+
+    void showItems() {
+        item1.display();
+        item2.display();
+    }
+};
+
+int main() {
+    Container box(10, 20);
+    box.showItems();      
+
+    return 0;
+}
