c504abad22c072cb989ca9a74a27fa625e4d761e
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-04-15 17:55:46 +0530

    Update Experiment-13.cpp

--- Experiments/Experiment-13.cpp
+++ Experiments/Experiment-13.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() {
+        cout << "Drawing a shape." << endl;
+    }
+
+    virtual void area() = 0; // Pure virtual function
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing a circle." << endl;
+    }
+
+    void area() override {
+        cout << "Area of circle = π * r * r" << endl;
+    }
+};
+
+int main() {
+    Shape* s = new Circle();
+    s->draw();   // Calls Circle's draw()
+    s->area();   // Calls Circle's area()
+
+    delete s;
+    return 0;
+}
