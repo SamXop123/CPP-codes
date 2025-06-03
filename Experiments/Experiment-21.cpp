f31d865842c14ec134b970e688d22e5613f46321
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-06-03 23:22:10 +0530

    Update Experiment-21.cpp

--- Experiments/Experiment-21.cpp
+++ Experiments/Experiment-21.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class PointerDemo {
+private:
+    int* ptr;
+
+public:
+    // Constructor to allocate memory
+    PointerDemo(int value) {
+        ptr = new int;   // dynamically allocate memory
+        *ptr = value;    // set value
+        cout << "Memory allocated and value set to " << *ptr << endl;
+    }
+
+    // Method to display the value
+    void display() {
+        cout << "Value: " << *ptr << endl;
+    }
+
+    // Method to change the value
+    void changeValue(int newValue) {
+        *ptr = newValue;
+    }
+
+    // Destructor to deallocate memory
+    ~PointerDemo() {
+        delete ptr;
+        cout << "Memory deallocated." << endl;
+    }
+};
+
+int main() {
+    PointerDemo obj(25);    // create object and set value to 25
+    obj.display();
+
+    obj.changeValue(100);   // change value to 100
+    obj.display();
+
+    return 0;
+}
 
