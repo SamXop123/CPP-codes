7cef2b06d8c488cffb38bc886b8cbb5315870537
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-06-05 15:49:48 +0530

    Update OOPS.cpp

--- OOPS/OOPS.cpp
+++ OOPS/OOPS.cpp
@@ -10,7 +10,7 @@ public:
 
     void introduce() {
         cout << "Hi, my name is " << name << ", and I am " << age << " years old." << endl;
-        cout << "I have a little knowledge about " << languages << "." << endl;
+        cout << "Languages I am familiar with: " << languages << "." << endl;
         cout << "In my free time, I enjoy " << hobbies << "." << endl;
     }
 };
@@ -18,9 +18,9 @@ public:
 int main() {
     Person p1;                       
     p1.name = "Sameer";            
-    p1.age = 18;                    
-    p1.languages = "Python, Java, HTML, CSS, C++";
-    p1.hobbies = "piano, photography, Minecraft, etc"; 
+    p1.age = 19;                    
+    p1.languages = "Python, Java, Javascript, C++";
+    p1.hobbies = "piano, photography, minecraft, football, etc"; 
     p1.introduce();                  
     return 0;
 }
