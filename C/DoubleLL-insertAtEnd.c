c127b18c8a7f4f4ef5ced270a2cfd4a4f687304f
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-08-25 22:07:46 +0530

    Create DoubleLL-insertAtEnd.c

---
+++ C/DoubleLL-insertAtEnd.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+        return head;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+void printListForward(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insertAtEnd(head, 10);
+    head = insertAtEnd(head, 20);
+    head = insertAtEnd(head, 30);
+
+    printf("Doubly linked list after insertion: ");
+    printListForward(head); // Output: 10 -> 20 -> 30 -> NULL
+
+    return 0;
+}
